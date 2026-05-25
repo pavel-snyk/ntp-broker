7261f925e8a41c6eb5ddbf9baa01529366acfc3c
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2026-05-26 01:10:32 +0200

    fix: publish via exec plugin and calling npm publish directly

--- release.config.cjs
+++ release.config.cjs
@@ -16,7 +16,11 @@ module.exports = {
       "@semantic-release/exec",
       {"prepareCmd": "mkdir -p sboms && npx snyk sbom --format spdx2.3+json > sboms/ntp-broker.sbom.spdx.json"}
     ],
-    "@semantic-release/npm",
+    ["@semantic-release/npm", {"npmPublish": false}],
+    [
+      "@semantic-release/exec",
+      {"publishCmd": "npm publish"}
+    ],
     [
       "@semantic-release/github",
       {
