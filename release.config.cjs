040a4df97963005d933e7441e5068bbe8dafc197
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2026-05-25 22:31:22 +0200

    fix: move prepublishOnly steps to exec plugin

--- release.config.cjs
+++ release.config.cjs
@@ -6,7 +6,33 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    // simulate pkg binary generation
+    [
+      "@semantic-release/exec",
+      {"prepareCmd": "npm run build && touch ntp-broker-linux"}
+    ],
+    // generate sboms
+    [
+      "@semantic-release/exec",
+      {"prepareCmd": "mkdir -p sboms && npx snyk sbom --format spdx2.3+json > sboms/ntp-broker.sbom.spdx.json"}
+    ],
     "@semantic-release/npm",
-    "@semantic-release/github",
+    [
+      "@semantic-release/github",
+      {
+        "assets": [
+          {
+            "path": "ntp-broker-linux",
+            "name": "ntp-broker-linux",
+            "label": "ntp-broker-linux"
+          },
+          {
+            "path": "sboms/ntp-broker.sbom.spdx.json",
+            "name": "ntp-broker.sbom.spdx.json",
+            "label": "ntp-broker.sbom.spdx.json"
+          }
+        ]
+      }
+    ]
   ]
 };
